fe14537d2a6d0f115f1072e9a2aa1eeabe91b9e6
mihir7573 <mihir2956@gmail.com>
2026-03-20 11:37:26 +0530

    Update assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -1,17 +1,17 @@
 let name = 'suresh'
 let age = 22
 let purchaseAmount = 100000
-
+let discount_percentage=0
 
 if(age <18){
-    console.log(discount_percentage +=10)
+    discount_percentage +=10
 
 }else if(age >=60){
-      console.log(discount_percentage +=20)
+      discount_percentage +=20
 }
 
 if(purchaseAmount > 5000){
-    console.log(purchaseAmount +=5)    
+    discount_percentage +=5   
 }
 
-console.log('Hii ' + name + '!' + 'You received a total discount of ' + age +  ' Final amount is ' + purchaseAmount )
+console.log(`Hii  ${name}    You received a total discount of ${discount_percentage}%    Final amount is  purchaseAmount `)
